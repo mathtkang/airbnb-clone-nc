0c98d92f5a4daef8d8cd52e6fb326ade6da9b555
[2기-1반] 강석영 <ksge1124@gmail.com>
2022-11-04 13:59:43 +0900

    using serializer

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Category
 
-# Create your views here.
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": serializers.serialize("json", all_categories),
+        }
+    )
